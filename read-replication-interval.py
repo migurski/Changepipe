424ae77622531bc95fbbcce5272f408932b31278
Michal Migurski <mike@stamen.com>
2012-01-22 17:54:31 -0800

    Changed everything to use ElementTree parser and Redis for storage

--- read-replication-interval.py
+++ read-replication-interval.py
@@ -3,38 +3,92 @@ from xml.parsers.expat import ParserCreate
 from xml.etree.ElementTree import parse
 from urllib import urlopen
 
-osmosis = 'osmosis --rri --simc --write-xml-change -'.split()
-osmosis = Popen(osmosis, stdout=PIPE)
+from redis import StrictRedis
 
-parser = ParserCreate()
-nodes, ways, changesets, unknowns = set(), set(), set(), set()
+expiration = 3600
 
-def start_element(name, attrs):
-    if name == 'node' and 'id' in attrs:
-        nodes.add(attrs['id'])
+def changed_elements(stream):
+    """
+    """
+    changes = parse(stream)
+    elements = []
     
-    if name == 'way' and 'id' in attrs:
-        ways.add(attrs['id'])
+    for change in changes.getroot().getchildren():
+        if change.tag in ('create', 'modify', 'delete'):
+            elements += change.getchildren()
     
-    if name == 'nd' and attrs['ref'] not in nodes:
-        unknowns.add(attrs['ref'])
-    
-    if 'changeset' in attrs:
-        changesets.add(attrs['changeset'])
+    return elements
 
-parser.StartElementHandler = start_element
-parser.ParseFile(osmosis.stdout)
+osmosis = 'osmosis --rri --simc --write-xml-change -'
+osmosis = Popen(osmosis.split(), stdout=PIPE)
 
+elements = changed_elements(osmosis.stdout)
 osmosis.wait()
 
-print len(nodes), 'nodes'
-print len(ways), 'ways'
-print len(unknowns), 'unknowns'
-print len(changesets), 'changesets:', ', '.join(changesets)
+nodes = [node for node in elements if node.tag == 'node']
+ways = [way for way in elements if way.tag == 'way']
+relations = [rel for rel in elements if rel.tag == 'relation']
 
-for changeset in changesets:
+# need: a list of changesets with their owners and extents that gets built up in redis
+# redis wants: a geofenced channel with a list of changeset ids in it, potentially of limited length.
+# changesets will build slowly over time, not using the API's bbox support because it's too blunt.
+# redis structure for a changeset - ?
+# ways: ask the API for their extent? keep them around with a timeout?
 
-    changeset = urlopen('http://api.openstreetmap.org/api/0.6/changeset/%s' % changeset)
-    changeset = parse(changeset).find('changeset').attrib
+redis = StrictRedis()
+
+for node in nodes:
+    #
+    # Save nodes to redis hashes under "node-id" keys, with keys "lat", "lon".
+    # Also add node keys to associated changeset redis set.
+    #
+    pipe = redis.pipeline(True)
+    node_key = 'node-%(id)s' % node.attrib
+    change_key = 'changeset-%(changeset)s' % node.attrib
+
+    pipe.hset(node_key, 'lat', node.attrib['lat'])
+    pipe.hset(node_key, 'lon', node.attrib['lon'])
     
-    print changeset['id'], changeset['user'], changeset['open']
+    pipe.sadd(change_key, node_key)
+    pipe.expire(node_key, expiration)
+    pipe.expire(change_key, expiration)
+    pipe.execute()
+
+for way in ways:
+    #
+    # Save ways to redis lists under "way-id-nodes" keys, with node id items.
+    # Also add way keys to associated changeset redis set.
+    #
+    pipe = redis.pipeline(True)
+    way_key = 'way-%(id)s' % way.attrib
+    way_nodes_key = way_key + '-nodes'
+    change_key = 'changeset-%(changeset)s' % node.attrib
+
+    pipe.delete(way_nodes_key)
+    for nd in way.findall('nd'):
+        pipe.rpush(way_nodes_key, nd.attrib['ref'])
+    
+    pipe.sadd(change_key, way_key)
+    pipe.expire(way_nodes_key, expiration)
+    pipe.expire(change_key, expiration)
+    pipe.execute()
+
+for relation in relations:
+    #
+    # Save relations to redis sets under "relation-id-members" keys, with 
+    # node-id and way-id members. Also add relation keys to associated 
+    # changeset redis set.
+    #
+    pipe = redis.pipeline(True)
+    relation_key = 'relation-%(id)s' % relation.attrib
+    relation_members_key = relation_key + '-members'
+    change_key = 'changeset-%(changeset)s' % relation.attrib
+
+    pipe.delete(relation_members_key)
+    for member in relation.findall('member'):
+        pipe.sadd(relation_members_key, '%(type)s-%(ref)s' % member.attrib)
+
+    pipe.sadd(change_key, relation_key)
+    pipe.expire(relation_members_key, expiration)
+    pipe.expire(change_key, expiration)
+    pipe.execute()
