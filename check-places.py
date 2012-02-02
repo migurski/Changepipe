7a5168b8dc2f2cb283403774049a910685f3653e
Michal Migurski <migurski@tilefarm.stamen.com>
2012-02-02 08:35:38 -0800

    Added bbox output for debugging

--- check-places.py
+++ check-places.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     for (index, line) in enumerate(open(argv[1])):
         name, geom = line.split('\t', 1)
         geom = wkt.loads(geom)
-        print 'line', index + 1, '-', name, '%.3f' % geom.area
+        print 'line', index + 1, '-', name, '%.3f' % geom.area,
+        xmin, ymin, xmax, ymax = geom.bounds
+        print '(%(ymax).3f, %(xmin).3f, %(ymin).3f, %(xmax).3f)' % locals()
